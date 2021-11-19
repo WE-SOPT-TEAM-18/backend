25cbb656c371bad94b2948791d0208174f186f53
변주현 <wngus4296@swu.ac.kr>
2021-11-19 19:31:52 +0900

    [UPDATE] init setting 수정

---
+++ functions/api/routes/user/userGET.js
@@ -0,0 +1,26 @@
+const functions = require('firebase-functions');
+const util = require('../../../lib/util');
+const statusCode = require('../../../constants/statusCode');
+const responseMessage = require('../../../constants/responseMessage');
+const db = require('../../../db/db');
+const { userDB } = require('../../../db');
+
+module.exports = async (req, res) => {
+  const { userId } = req.params;
+  if (!userId) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+
+  let client;
+
+  try {
+    client = await db.connect(req);
+
+    const user = await userDB.getUserById(client, userId);
+    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_ONE_USER_SUCCESS, user));
+  } catch (error) {
+    functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
+    console.log(error);
+    res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+  } finally {
+    client.release();
+  }
+};
