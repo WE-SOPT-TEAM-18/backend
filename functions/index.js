4ee69cc713d1d8ecaea41b8b35b544ef324983bf
변주현 <wngus4296@swu.ac.kr>
2021-11-19 17:26:42 +0900

    [FEAT] server init setting

---
+++ functions/index.js
@@ -0,0 +1,18 @@
+const admin = require("firebase-admin");
+const serviceAccount = require("./firebase account");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+let firebase;
+if (admin.apps.length === 0) {
+  firebase = admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+} else {
+  firebase = admin.app();
+}
+
+module.exports = {
+  api: require("./api"),
+};
