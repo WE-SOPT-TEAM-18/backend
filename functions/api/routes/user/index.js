50cb4e7a70efdf52b349cae3d67319ee01bdff9b
변주현 <wngus4296@swu.ac.kr>
2021-11-24 17:04:45 +0900

    [REMOVE] 필요없는 파일 삭제

--- functions/api/routes/user/index.js
+++ functions/api/routes/user/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-// '/user/:userId'로 오는 요청을 userGET 파일에서 처리
 router.get('/:userId', require('./userGET'));
 
 module.exports = router;
