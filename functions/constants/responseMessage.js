ae62fed8a280cc68b12d15328e18c88374104224
변주현 <wngus4296@swu.ac.kr>
2021-11-24 18:29:07 +0900

    [UPDATE] responseMessage update

--- functions/constants/responseMessage.js
+++ functions/constants/responseMessage.js
@@ -2,39 +2,16 @@ module.exports = {
   NULL_VALUE: '필요한 값이 없습니다',
   OUT_OF_VALUE: '파라미터 값이 잘못되었습니다',
 
-  // 회원가입
-  CREATED_USER: '회원 가입 성공',
-  DELETE_USER: '회원 탈퇴 성공',
-  ALREADY_EMAIL: '이미 사용중인 이메일입니다.',
+  // 좋아요
+  READ_LIKE_CONTENTS_SUCCESS: '좋아요 누른 컨텐츠 조회 성공',
+  ADD_LIKE_CONTENTS_SUCCESS: '컨텐츠 좋아요 성공',
 
-  // 로그인
-  LOGIN_SUCCESS: '로그인 성공',
-  LOGIN_FAIL: '로그인 실패',
-  NO_USER: '존재하지 않는 회원입니다.',
-  MISS_MATCH_PW: '비밀번호가 맞지 않습니다.',
-
-  // 프로필 조회
-  READ_PROFILE_SUCCESS: '프로필 조회 성공',
-
-  // 유저
-  READ_ONE_USER_SUCCESS: '유저 조회 성공',
-  READ_ALL_USERS_SUCCESS: '모든 유저 조회 성공',
-  UPDATE_ONE_USER_SUCCESS: '유저 수정 성공',
-  DELETE_ONE_USER_SUCCESS: '유저 삭제 성공',
-
-  // 포스트
-  ADD_ONE_POST_SUCCESS: '포스트 추가 성공',
-  READ_ONE_POST_SUCCESS: '포스트 조회 성공',
-  READ_ALL_POSTS_SUCCESS: '모든 포스트 조회 성공',
-  UPDATE_ONE_POST_SUCCESS: '포스트 수정 성공',
-  DELETE_ONE_POST_SUCCESS: '포스트 삭제 성공',
-  NO_POST: '존재하지 않는 포스트입니다.',
+  // 컨텐츠
+  READ_ALL_CONTENTS_SUCCESS: '컨텐츠 조회 성공',
+  READ_CONTENTS_RANK_SUCCESS: '컨텐츠 랭킹 조회 성공',
+  READ_CATEGORY_CONTENTS_SUCCESS: '카테고리별 컨텐츠 조회 성공',
+  READ_WATCHING_CONTENTS_SUCCESS: '시청 중인 콘텐츠 가져오기 성공',
 
   // 서버 내 오류
   INTERNAL_SERVER_ERROR: '서버 내 오류',
-
-  // 토큰
-  TOKEN_EXPIRED: '토큰이 만료되었습니다.',
-  TOKEN_INVALID: '토큰이 유효하지 않습니다.',
-  TOKEN_EMPTY: '토큰이 없습니다.',
 };
