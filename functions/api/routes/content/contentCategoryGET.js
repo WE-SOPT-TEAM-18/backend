c5445e1bbb3abc9529fc9d45600dcde1e162ab3c
변주현 <wngus4296@swu.ac.kr>
2021-11-25 04:09:06 +0900

    [FEAT] contentCategoryGET 구현

---
+++ functions/api/routes/content/contentCategoryGET.js
@@ -0,0 +1,29 @@
+const functions = require('firebase-functions');
+const util = require('../../../lib/util');
+const statusCode = require('../../../constants/statusCode');
+const responseMessage = require('../../../constants/responseMessage');
+const categoryNum = require('../../../constants/category');
+const db = require('../../../db/db');
+const { contentDB } = require('../../../db');
+
+module.exports = async (req, res) => {
+  const { category } = req.params;
+  if ( !category ) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+  
+  try {
+    client = await db.connect(req);
+
+    const contents = await contentDB.getContentByCategory(client, category);
+    const data = {
+        "category": categoryNum[category - 1],
+        "contents": contents,
+    };
+    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_CATEGORY_CONTENTS_SUCCESS, data));
+  } catch (error) {
+    functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
+    console.log(error);
+    res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+  } finally {
+    client.release();
+  }
+};
