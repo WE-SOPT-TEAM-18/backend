43b3325191ca0ea06916bc9d6dc7c23a7ce38ca5
choiyoorim <yulim3839@naver.com>
2021-11-26 01:53:38 +0900

    [FEAT] likeGET, likePOST 구현

--- functions/api/routes/index.js
+++ functions/api/routes/index.js
@@ -3,5 +3,5 @@ const router = express.Router();
 
 router.use('/watching', require('./watching'));
 router.use('/content', require('./content'));
-
+router.use('/like',require('./like'));
 module.exports = router;
